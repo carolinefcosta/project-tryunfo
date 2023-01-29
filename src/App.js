872fefed9c67b917168e37b324329cfbea6ca50c
Caroline <carolinefernandesc@hotmail.com>
2023-01-29 11:44:44 -0300

    requisito 05

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ class App extends React.Component {
     super();
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.verifyInputsText = this.verifyInputsText.bind(this);
+    this.verifyCardAttr = this.verifyCardAttr.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
 
     this.state = {
       cardName: '',
       cardDescription: '',
-      cardAttr1: '',
-      cardAttr2: '',
-      cardAttr3: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
       cardImage: '',
       cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
+      isSaveButtonDisabled: true,
+
     };
   }
 
@@ -27,8 +32,45 @@ class App extends React.Component {
 
     this.setState({
       [name]: value,
+    }, () => {
+      if (this.verifyCardAttr() && this.verifyInputsText()) {
+        this.setState({
+          isSaveButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isSaveButtonDisabled: true,
+        });
+      }
     });
   };
+
+  onSaveButtonClick() {
+  }
+
+  verifyCardAttr() {
+    const {
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    const total = 210;
+    return ((Number(cardAttr1) >= 0 && Number(cardAttr1) <= 90)
+      && (Number(cardAttr2) >= 0 && Number(cardAttr2) <= 90)
+      && (Number(cardAttr3) >= 0 && Number(cardAttr3) <= 90)
+      && (Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= total));
+  }
+
+  verifyInputsText() {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+    } = this.state;
+    return (cardName !== ''
+    && cardDescription !== '' && cardImage !== '' && cardRare !== '');
+  }
 
   render() {
     const {
@@ -41,6 +83,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
+      isSaveButtonDisabled,
     } = this.state;
     return (
       <div>
@@ -56,6 +99,8 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
           hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onSaveButtonClick={ this.onSaveButtonClick }
         />
         <Card
           onInputChange={ this.onInputChange }
